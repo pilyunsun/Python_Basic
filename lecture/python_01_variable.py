613e47026793fa0797b4e767991a75cab40df7dd
pilyunsun <127662556+pilyunsun@users.noreply.github.com>
2023-09-08 12:51:38 +0900

    Add python_01_variable

---
+++ lecture/python_01_variable.py
@@ -0,0 +1,16 @@
+# 주석(comment) : 단순 메모, 실행 x
+
+# 출력 함수(print)
+ # - ()안의 갑을 출력
+ # - 변수 값 확인 용도 또는 메세지 출력 용도
+
+print("hello world")
+
+ # 문자열 타입(string Type)
+# - python: '',"" ->string
+# - c,java:''(char),""(string)
+# -문번: \(역슬러시) + @
+# - 문자열 내의 일부 문자의 의미를 달리하여 특정한 효과 주기
+# - 예) \n: 한 줄 개행, \t:탭 (8칸 공백)
+print("hello \nPython");
+print("hello \tPython");
