8e67c19c144d17ebb12be4bcedb0819eb1e660b4
pilyunsun <127662556+pilyunsun@users.noreply.github.com>
2023-09-08 12:51:38 +0900

    Add python_01_variable

---
+++ lecture/python_01_variable.py
@@ -0,0 +1,11 @@
+# 주석(comment) : 단순 메모, 실행 x
+
+# 출력 함수(print)
+ # - ()안의 갑을 출력
+ # - 변수 값 확인 용도 또는 메세지 출력 용도
+
+print("hello world")
+
+ # 문자열 타입(string Type)
+# - python: '',"" ->string
+# - c,java:''(char),""(string)
